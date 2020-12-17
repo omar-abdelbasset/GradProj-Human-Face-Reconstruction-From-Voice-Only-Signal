939e52c9a77a6c3dc627da7b93079854ea887446
KhaledAtefMansour <44950887+KhaledAtefMansour@users.noreply.github.com>
2020-12-17 23:33:14 +0200

    Update Audio_Input.py

--- Audio Preprocessing/Audio_Input.py
+++ Audio Preprocessing/Audio_Input.py
@@ -8,26 +8,33 @@ import matplotlib.pyplot as plt
 
 class Audio_Preprocessor:
 
-	# Preprocessing Parameters
-	Sampling_Rate = 16000
-	Frame_Size = 512
-	Hop_Length = 160
-	Hann_Window = 400
+	def __init__(self, wav_path = "",Processed_path="",sr = 16000, frSize = 512, HopLength = 160, HannWindow = 400):
+		# Preprocessing Parameters
+		self.Processed_path ="Processed Audio\\"
+
+		if not os.path.exists(self.Processed_path):	
+			os.makedirs(self.Processed_path,mode=0o777,exist_ok = False)
+		
+		self.Sampling_Rate = sr
+		self.Frame_Size = frSize
+		self.Hop_Length = HopLength
+		self.Hann_Window = HannWindow
 
 	def Complex_Spectrogram(self,Audio_Path):
 
 		# load Audio File
-		audio, sr = librosa.load(Audio_Path,sr=self.Sampling_Rate,mono=True)
-
+		audio, sr = librosa.load(Audio_Path,sr=self.Sampling_Rate,mono=True,duration=6.0)
 		# check the Audio duration 
 		if(audio.shape[0] < self.Sampling_Rate*6):
+			print("Smaller")
 			audio = np.resize(audio,(self.Sampling_Rate*6,))
 		
 		elif(audio.shape[0] > self.Sampling_Rate*6):
-			audio = audio[0:self.Sampling_Rate*6]
-
+			print("bigger")
+			audio = audio[0:self.Sampling_Rate*6-1]
+		
 		# calculate the STFT
-		Frequency_Components = librosa.stft(audio, n_fft = self.Frame_Size, hop_length = self.Hop_Length, win_length = self.Hann_Window )
+		Frequency_Components = librosa.stft(audio, n_fft = self.Frame_Size, hop_length = self.Hop_Length, win_length = self.Hann_Window, window = "hann",center=False )
 
 		print(Frequency_Components.shape)
 
@@ -58,14 +65,37 @@ class Audio_Preprocessor:
 		plt.colorbar(format="%+2.f")
 		plt.show()
 
+	def write(self, Real_Part, Imaginary_Part, Name):
+		
+		output_Path = self.Processed_path + Name + ".txt"
+		#with open(output_Path,"w+") as output:
+		np.savetxt(output_Path,np.r_[Real_Part,Imaginary_Part])
 
+	def read(self, Name):
+		input_Path = Name + ".txt"
+		real=[]
+		imaginary=[]
+		counter = 0
+		with open(input_Path,"r") as file:
+			for line in file:
+				if(counter<257):
+					real.append(np.float_(line.split(' ')))
+				else:
+					imaginary.append(np.float_(line.split(' ')))
+				counter= counter+1
+		return real,imaginary
 
 # Testing
 Preprocessor = Audio_Preprocessor()
 
 # Put the Audio File Path
-Audio_Path = " "
+Audio_Path = "E:\\GraduationProject\\Code\\AVDataset\\audios\\_2Rjfow5gow.wav"
 
 Real_Part,Imaginary_Part =Preprocessor.Complex_Spectrogram(Audio_Path)
+
+print(Real_Part.shape,Imaginary_Part.shape)
 Preprocessor.Draw_Spectrograms(Real_Part,Imaginary_Part)
+
+
+
 
